0a176e0f27d0adf22c2033cb48f7741e30e77069
Noah Ewell <noahewell.life@gmail.com>
2024-07-31 19:03:13 -0600

    Experimenting with targeting DOM elements via JavaScript

--- Web Development Projects/DOM/index.js
+++ Web Development Projects/DOM/index.js
@@ -1,2 +1,5 @@
-var third = document.querySelector("ul").lastElementChild;
-third.innerHTML = "Noah";
+document.querySelector("a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").classList.toggle("huge");
